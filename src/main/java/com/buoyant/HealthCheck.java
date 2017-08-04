8717ad67d531f471e449a5f0c7eb8e0380ad0d15
Priyanka Kulkarni <piyu@priyankas-air.gateway.sonic.net>
2017-08-04 13:49:00 -0700

    updated HealthCheck and pom to use servlets

--- src/main/java/com/buoyant/HealthCheck.java
+++ src/main/java/com/buoyant/HealthCheck.java
@@ -1,11 +1,16 @@
 package com.buoyant;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
 
 public class HealthCheck{
 	public static void main(String[] args) throws Exception
     {
         Server server = new Server(8080);
-        server.setHandler(new HealthCheckHandler());
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+        handler.addServletWithMapping(Handler2.class, "/");
+
+        
  
         server.start();
         server.join();
